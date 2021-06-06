4e63b06a05a603c8129f646580e8bf0b88a70fb6
Ronak2001 <Ronak.01@outlook.com>
2021-06-06 12:50:05 -0400

    Adding images to React app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import './App.css';
+import lengineer from "./Hulahooh.JPG"
 
-function Header(){
+function Header(props){
   return (
     <header> 
-    <h1>Ronak's Code</h1>
+    <h1>{props.name} is awsome!</h1>
     </header>
   );
 }
 
+function Main(props){
+  return(
+    <section>
+      <p>Roank was {props.verb}. Ronak is {props.verb}. Ronak will be {props.verb}.</p> 
+      <img src={lengineer} height={200} alt="LeNgineer Logo"/>
+      <img src="https://github.com/ronak2001.png" height={200} alt="Ronak's github profile"/>
+
+      <ul style={{textAlign:"left"}}>
+        {props.languages.map((language)=>(
+        <li key={language.id}>{language.title}</li>
+        ))}
+      </ul>
+
+    </section>
+  );
+}
+
+function Footer(props){
+  return(
+    <p>He does not know anything else as till {props.year}.</p>
+  );
+}
+
+const languages = [
+  "Java",
+  "Python", 
+  "JavaScript", 
+  "C", 
+  "C++"
+];
+
+const languageObject = languages.map((language,i)=>
+({id:i, title:language}));
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <h2>Main</h2>
-      <h3>Fotter</h3>
+      <Header name="Ronak"/>
+      <Main verb="coding" languages={languageObject}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
