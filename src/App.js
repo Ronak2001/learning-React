43e2374860204535bf1ffad83112695b80230bae
Ronak2001 <Ronak.01@outlook.com>
2021-06-05 23:43:10 -0400

    creating webpage Componants

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(){
+  return (
+    <header> 
+    <h1>Ronak's Code</h1>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <h2>Main</h2>
+      <h3>Fotter</h3>
     </div>
   );
 }
