9947f568bea870baf6d7307438ab412207e3f7ed
Ronak Patel <Ronak.01@outlook.com>
2022-04-16 14:34:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import lengineer from "./Hulahooh.JPG"
 
-function Header(props){
+function Header(props) {
   return (
-    <header> 
-    <h1>{props.name} is awsome!</h1>
+    <header>
+      <h1>{props.name} is {props.adjective}!</h1>
     </header>
   );
 }
 
-function Main(props){
-  return(
+function Main(props) {
+  return (
     <section>
-      <p>Roank was {props.verb}. Ronak is {props.verb}. Ronak will be {props.verb}.</p> 
-      <img src={lengineer} height={200} alt="LeNgineer Logo"/>
-      <img src="https://github.com/ronak2001.png" height={200} alt="Ronak's github profile"/>
+      <p>Roank was {props.verb}. Ronak is {props.verb}. Ronak will be {props.verb}.</p>
+      <img src={lengineer} height={200} alt="LeNgineer Logo" />
+      <img src="https://github.com/ronak2001.png" height={200} alt="Ronak's github profile" />
 
-      <ul style={{textAlign:"left"}}>
-        {props.languages.map((language)=>(
-        <li key={language.id}>{language.title}</li>
+      <ul style={{ textAlign: "left" }}>
+        {props.languages.map((language) => (
+          <li key={language.id}>{language.title}</li>
         ))}
       </ul>
 
@@ -26,34 +27,44 @@ function Main(props){
   );
 }
 
-function Footer(props){
-  return(
+function Footer(props) {
+  return (
     <p>He does not know anything else as till {props.year}.</p>
   );
 }
 
 const languages = [
   "Java",
-  "Python", 
-  "JavaScript", 
-  "C", 
+  "Python",
+  "JavaScript",
+  "C",
   "C++"
 ];
 
-const languageObject = languages.map((language,i)=>
-({id:i, title:language}));
+const languageObject = languages.map((language, i) => ({ id: i, title: language }));
+
 
 function App(props) {
-  if(props.authorized){
-  return (
-    <div className="App">
-      <Header name="Ronak"/>
-      <Main verb="coding" languages={languageObject}/>
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+
+const [adjective, setAdjective] = useState("awesome");
+
+  if (props.authorized) {
+    return (
+      <div className="App">
+        <Header name="Ronak" adjective={adjective} />
+        <>
+        <button onClick={() => setAdjective("amazing")} >Amazing</button>
+        <button onClick={() => setAdjective("fabulous")}>Fabulous</button>
+        <button onClick={() => setAdjective("still awesome")}>Crazy</button>
+        </>
+
+
+        <Main verb="coding" languages={languageObject} />
+        <Footer year={new Date().getFullYear()} />
+      </div>
+    );
   }
-  else{
+  else {
     return (null)
   }
 }
