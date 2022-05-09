19c23db5c749d3edb45ba4218ea2d933f064d73b
Ronak Patel <Ronak.01@outlook.com>
2022-05-08 21:57:06 -0400

    using JSX

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -5,12 +5,10 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // 1st argument is element name, 2nd is element attribute and 3rd,4th ...nth is anything between start and end tags
-  React.createElement(
-    "ul",
-    null,
-    React.createElement("li", null, "January"),
-    React.createElement("li", null, "Fabuary"),
-    React.createElement("li", null, "March")
-  ),
+  //using JSX
+  <ul>
+    <li>January</li>
+    <li>Fabuary</li>
+    <li>March</li>
+  </ul>
 );
